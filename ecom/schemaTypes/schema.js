e24af9770db1ef9ab6b32acc35b2005cfcb984a6
Klajdi2004 <klajdi.dhana1@gmail.com>
2024-03-09 22:20:18 +0100

    /

--- ecom/schemaTypes/schema.js
+++ ecom/schemaTypes/schema.js
@@ -1,8 +1,8 @@
 import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
-import product from './product';
-import banner from './banner';
+import product from './product.js';
+import banner from './banner.js';
 
 export default createSchema({
   name: 'default',
